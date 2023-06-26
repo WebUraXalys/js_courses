232f14197bdc97cc306faf823f8740a1a0fdee15
xalys_ura <xalysjyra1992@gmail.com>
2023-06-26 19:50:41 +0300

    Ansver

--- 25/tempCodeRunnerFile.js
+++ 25/tempCodeRunnerFile.js
@@ -1,18 +1,17 @@
 function getMathResult(num, progres){
+   // if(typeof(num) != "number" || typeof(progres) != "number") return num;
+   // if(progres <= 0) return num;
+   if(typeof(num) !== "number" || typeof(progres) !== "number" || progres <= 0) return num;
    let str = "";
-   if(typeof(num) != "number" || typeof(progres) != "number"){
-      str = num;
-      return str;
-   } else{
-   if(progres <= 0) return num;
    for(let i = 1; i <= progres; i++){
       if(i == progres){
-         str = str + (num * i);
+         // str = str + (num * i);
+         str += num*i
       } else{
-         str = str + (num * i) + "---";
+         // str = str + (num * i) + "---";
+         str += `${num * i}---`;
       }
    }
    return str;
-   };
 };
-console.log(getMathResult(10, -4));
+console.log(getMathResult(10, 4));
--- 25/test.js
+++ 25/test.js
@@ -14,16 +14,19 @@ console.log(returnNeighboringNumbers(5));
 
 // Место для третьей задачи
 function getMathResult(num, progres){
-   if(typeof(num) != "number" || typeof(progres) != "number") return num;
-   if(progres <= 0) return num;
+   // if(typeof(num) != "number" || typeof(progres) != "number") return num;
+   // if(progres <= 0) return num;
+   if(typeof(num) !== "number" || typeof(progres) !== "number" || progres <= 0) return num;
    let str = "";
    for(let i = 1; i <= progres; i++){
       if(i == progres){
-         str = str + (num * i);
+         // str = str + (num * i);
+         str += num*i
       } else{
-         str = str + (num * i) + "---";
+         // str = str + (num * i) + "---";
+         str += `${num * i}---`;
       }
    }
    return str;
 };
-console.log(getMathResult(10, -4));
+console.log(getMathResult(10, 4));
